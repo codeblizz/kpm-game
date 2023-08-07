b415b4b4174e604c1d2bc8d9c89e6435ee6ad0c1
collins.dev <codeblizzen@gmail.com>
2023-08-07 17:08:28 +0400

    update: utils style mapper for bet slip

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,5 +1,14 @@
 
 
-const utils = {};
+const utils = {
+    extraPropsMapper: (infoMsg: { msg: any }) => {
+        return {
+          backgroundColor: 'bg-white',
+          titleColor: infoMsg.msg ? `bg-[#008000]` : `bg-[#ff0000]`,
+          descriptionColor: infoMsg.msg ? `bg-[#008000]` : `bg-[#ff0000]`,
+          buttonBGColor: infoMsg.msg ? 'bg-black' : `bg-black`,
+        };
+      },
+};
 
 export default utils;
